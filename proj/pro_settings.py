8c7acbf50a6926bf9d4020800526b3a9b9f4fb0e
John Lamb <johnlamb@pcons1.scilifelab.se>
2017-08-11 12:12:01 +0200

    Production settings done, now synced github repo against prod

---
+++ proj/pro_settings.py
@@ -0,0 +1,91 @@
+"""
+Django settings for proj project in production
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+DATA_DIR = os.path.join(BASE_DIR,'../web_pconsfold/static/data')
+#try:
+#    from shared_settings import *
+#except ImportError:
+#    pass
+
+with open('/etc/django_pro_secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['*']
+
+#computenodefile = "%s/pred/static/computenode.txt"%(BASE_DIR)
+#if os.path.exists(computenodefile):
+#    nodelist = []
+#    try:
+#        nodelist = open(computenodefile, "r").read().split()
+#    except:
+#        pass
+#   ALLOWED_HOSTS += nodelist
+INSTALLED_APPS = (
+        'web_pconsfold',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'proj.urls'
+
+WSGI_APPLICATION = 'proj.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
