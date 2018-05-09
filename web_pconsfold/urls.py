3185c0a6b931b3e5ad333cf449f36e7d67a5831c
John Lamb <johnlamb@pcons1.scilifelab.se>
2018-05-09 14:58:40 +0200

    Bringing everything up to date

--- web_pconsfold/urls.py
+++ web_pconsfold/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import patterns, url
 
 from web_pconsfold import views
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),
         url(r'(?P<pdb_id>[PF]{2}[0-9]{5}.[0-9]{1,2})/$', views.details, name='details'),
+        url(r'help$', views.get_help, name='get_help'),
+        url(r'news$', views.get_news, name='get_news'),
+        url(r'download$', views.get_download, name='get_download'),
+        url(r'reference$', views.get_reference, name='get_reference'),
+        url(r'^$', views.index, name='index'),
+        # url(r'^news/$', views.get_news, name='get_news'),
         )
--- web_pconsfold/views.py
+++ web_pconsfold/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
+# from filebrowser.base import FileListing
 import os
 # Create your views here.
 def index(request):
@@ -8,9 +9,32 @@ def index(request):
     listFile = open(os.path.join(settings.DATA_DIR,'list.txt'),'r')
     headers = listFile.readline().strip().split(',')
     for line in listFile:
-        tableData.append(line.strip().split(','))
+	temp_string = '<tr><td>'
+	splitline = line.strip().split(",")
+	# <td><a href="/details/{{ cell }}">{{ cell }}</a> </td>
+	temp_string += '<a href="/details/' + splitline[0] + '">' + splitline[0] + '</a></td><td>'
+	temp_string += "</td><td>".join(splitline[1:]) + "</td></tr>"
+        tableData.append(temp_string)
     return render(request, 'web_pconsfold/index.html',{'headers':headers, 'tableData': tableData})
 
 def details(request,pdb_id):
     modelURL = os.path.join(settings.STATIC_URL, 'data/29.0/'+pdb_id+'/model.pdb')
-    return render(request, 'web_pconsfold/details.html',{'pdb_id':pdb_id, 'modelURL':modelURL})
+    base_url = os.path.join(settings.STATIC_URL, 'data/29.0/', pdb_id)
+    ls_list = []
+    for f in os.listdir('/big/pfam/web_pconsfold/static/data/29.0/' + pdb_id):
+	ls_list.append((f, os.path.join(base_url, f)))
+    return render(request, 'web_pconsfold/details.html',{'pdb_id':pdb_id, 'modelURL':modelURL, 'base_url':base_url, 'ls_list':ls_list})
+
+
+def get_help(request):
+    # return HttpResponse('In help site')
+    return render(request, 'web_pconsfold/help.html', {})
+def get_news(request):
+    # return HttpResponse('In help site')
+    return render(request, 'web_pconsfold/news.html', {})
+def get_download(request):
+    # return HttpResponse('In help site')
+    return render(request, 'web_pconsfold/download.html', {})
+def get_reference(request):
+    # return HttpResponse('In help site')
+    return render(request, 'web_pconsfold/reference.html', {})
