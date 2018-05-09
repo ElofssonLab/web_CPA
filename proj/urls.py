3146093644287529c905d8d2f2bc065bbd5c96fb
John Lamb <johnlamb@pcons1.scilifelab.se>
2018-05-09 15:00:18 +0200

    Bringing everything up to date

--- proj/urls.py
+++ proj/urls.py
@@ -7,6 +7,11 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', include('web_pconsfold.urls', namespace='web_pconsfold')),
     url(r'^details/', include('web_pconsfold.urls', namespace='web_pconsfold')),
+    url(r'^pred/', include('web_pconsfold.urls', namespace='web_pconsfold')),
+    # url(r'^help/', include('web_pconsfold.urls', namespace='web_pconsfold')),
+    # url(r'^news/', include('web_pconsfold.urls', namespace='web_pconsfold')),
+    # url(r'^reference/', include('web_pconsfold.urls', namespace='web_pconsfold')),
+    # url(r'^download/', include('web_pconsfold.urls', namespace='web_pconsfold')),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
