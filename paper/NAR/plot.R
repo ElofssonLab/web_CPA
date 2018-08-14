4878d4757c9bf9eb7cdf5db10a5ea574b55c1db2
Arne Elofsson <arne@bioinfo.se>
2018-08-14 15:51:29 +0200

    By syncing

--- paper/NAR/plot.R
+++ paper/NAR/plot.R
@@ -40,3 +40,31 @@ plot(nostructFDR,main="FDR", xlab="Fdr", ylab="Density",col="red")
 lines(structFDR,col="blue")
 legend(0.1,3, legend=c("Structure", "No structure"), col=c("blue", "red"), lty=1:2, cex=0.8)
 dev.off()
+
+
+ma <- function(x,n=5000){filter(x,rep(1/n,n), sides=2)}
+
+library(scales)
+
+
+
+#postscript("figures/meff-pcons.eps", width=8,height=8,paper="special",horizontal=F,onefile=F,colormodel="rgb")
+cairo_ps(file = "figures/meff-pcons.eps", onefile = FALSE, fallback_resolution = 600)
+plot(log10(structure$Meff),structure$pcons,,cex=0.6,col = alpha("lightblue", 0.4),xlab="log(Meff)",ylab="Pcons")
+points(log10(nostruct$Meff),nostruct$pcons,cex=0.6,pch=0,col = alpha("pink", 0.4))
+points(log10(structure$Meff[order(structure$Meff)]),ma(structure$pcons[order(structure$Meff)]), col=alpha("blue",0.2))
+points(log10(nostruct$Meff[order(nostruct$Meff)]),ma(nostruct$pcons[order(nostruct$Meff)]), col=alpha("red",0.2))
+legend(1,0.7, legend=c("Structure", "No structure"), col=c("blue", "red"), lty=1:2, cex=0.8)
+dev.off()
+
+
+
+#postscript("figures/meff-pcons.eps", width=8,height=8,paper="special",horizontal=F,onefile=F,colormodel="rgb")
+cairo_ps(file = "figures/meff-proq3d.eps", onefile = FALSE, fallback_resolution = 600)
+plot(log10(structure$Meff),structure$proq,,cex=0.6,col = alpha("lightblue", 0.4),xlab="log(Meff)",ylab="ProQ3d")
+points(log10(nostruct$Meff),nostruct$proq,cex=0.6,pch=0,col = alpha("pink", 0.4))
+points(log10(structure$Meff[order(structure$Meff)]),ma(structure$proq[order(structure$Meff)]), col=alpha("blue",0.2))
+points(log10(nostruct$Meff[order(nostruct$Meff)]),ma(nostruct$proq[order(nostruct$Meff)]), col=alpha("red",0.2))
+legend(1,0.7, legend=c("Structure", "No structure"), col=c("blue", "red"), lty=1:2, cex=0.8)
+dev.off()
+
