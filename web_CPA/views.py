6663e946c4afee5b0f70fe68bab3e3f91e05b2e8
John Lamb <lamb.john.n@gmail.com>
2020-09-10 15:25:46 +0200

    Final layout is progressing

--- web_CPA/views.py
+++ web_CPA/views.py
@@ -30,7 +30,7 @@ def details(request, subfamily_id):
     # dmapURL = os.path.join(settings.STATIC_URL, 'data/29.0/'+pfam_id+'/model.dmap')
     # # dmapURLs = [dmapURL]
     # base_url = os.path.join(settings.DATA_DIR, '29.0/', pfam_id)
-    base_url = os.path.join(settings.DATA_DIR, 'CPA/', subfamily_id)
+    # base_url = os.path.join(settings.DATA_DIR, 'CPA/', subfamily_id)
     cpa_url = os.path.join(settings.DATA_DIR, 'CPA/', subfamily_id)
     ls_list = []
     fasta_url = ''
@@ -38,6 +38,9 @@ def details(request, subfamily_id):
     # prot_di = fix(id,glob.glob("{}/*.l3".format(jobDir)))
     di_list = []
     model_list = []
+    Full_alignment_images_raw = glob.glob(settings.DATA_DIR + "/CPA/images/Full_length_seq_topo_figures/*" + subfamily_id + "*")
+    Repeat_alignment_images_raw = glob.glob(settings.DATA_DIR + "/CPA/images/Repeat_seq_topo_figures/*" + subfamily_id + "*")
+    # Repeat_alignment_images = glob.glob(cpa_url + "/*.pdb")
     cpa_models = glob.glob(cpa_url + "/*.pdb")
     pir_models = glob.glob(cpa_url + "/*.pir")
     seed_models_raw = glob.glob(cpa_url + "/*-seed.msa")
@@ -46,10 +49,17 @@ def details(request, subfamily_id):
     buttons_path = glob.glob(cpa_url + "/*_buttons.txt")
     topAnnot_names = glob.glob(cpa_url + "/*-topology_annotation.svg")  # [0].split('/')[-1]
     # if len(cpa_models) > 0:
+    # print(cpa_models)
+    # print(pir_models)
     cpa_model = os.path.join(settings.STATIC_URL, 'data/CPA/', subfamily_id, cpa_models[0].split("/")[-1])
-    pir_model = os.path.join(settings.STATIC_URL, 'data/CPA/', subfamily_id, pir_models[0].split("/")[-1])
+    if len(pir_models) > 0:
+        pir_model = os.path.join(settings.STATIC_URL, 'data/CPA/', subfamily_id, pir_models[0].split("/")[-1])
+    else:
+        pir_model = ''
     seed_models = [os.path.join(settings.STATIC_URL, 'data/CPA/', subfamily_id, seed_model.split("/")[-1]) for seed_model in seed_models_raw]
     fasta_topo_file = [os.path.join(settings.STATIC_URL, 'data/CPA/', subfamily_id, fasta_topo_file.split("/")[-1]) for fasta_topo_file in fasta_topo_files][0]
+    Full_alignment_images = [os.path.join(settings.STATIC_URL, 'data/CPA/images/Full_length_seq_topo_figures', full_image.split("/")[-1]) for full_image in Full_alignment_images_raw]
+    Repeat_alignment_images = [os.path.join(settings.STATIC_URL, 'data/CPA/images/Repeat_seq_topo_figures', repeat_image.split("/")[-1]) for repeat_image in Repeat_alignment_images_raw]
     # print(fasta_topo_file)
     
     ######################
@@ -73,9 +83,9 @@ def details(request, subfamily_id):
             else:
                 info[key] = value
     if "_struct.pdb" in cpa_model:
-        model_type = "Known structure"
+        model_type = "Crystal structure"
     else:
-        model_type = "Model structure"
+        model_type = "Homology structure"
     cartoon_url = os.path.join(settings.STATIC_URL, 'data/CPA/', subfamily_id, subfamily_id + "-cartoon.svg")
     krbias_url = os.path.join(settings.STATIC_URL, 'data/CPA/', subfamily_id, subfamily_id + "-KRbias.png")
     motif_url = os.path.join(settings.STATIC_URL, 'data/CPA/', subfamily_id, subfamily_id + "-NC_CC.png")
@@ -102,25 +112,33 @@ def details(request, subfamily_id):
     # print(buttons_text)
     # raw_models = glob.glob(base_url + "/*.pdb")
     # raw_DIs = glob.glob(base_url + "/*.l3")
-    # fasta_url = glob.glob(base_url + "/*.fa")[0]
+    temp_fasta_url = glob.glob(cpa_url + "/" + subfamily_id +".fasta")[0]
     # desc_file = glob.glob(base_url + "/description.txt")[0]
 
+    fa_len = 0
+    with open(temp_fasta_url) as fa_handle:
+        fa_handle.readline()
+        seq = ''.join(fa_handle.readlines())
+        # print(seq)
+        fa_len = len(seq)
+        # print seq
+        # print len(seq)
     # topology
     topo_lambda = lambda x: x.split("/")[0][:-1]+"<sub>"+x.split("/")[0][-1]+"</sub>"
 
-    topology_calculated = 0
-    topo_data = [[],[],[],"",""]
-    topo_file = glob.glob(base_url + "/topology.txt")
-    if topo_file:
-        topology_calculated = 1
-        topo_file = topo_file[0]
-        with open(topo_file) as topo_in:
-            topo_data = topo_in.readlines()
-        topo_data[0] = map(topo_lambda,topo_data[0].strip().split(";"))
-        topo_data[1] = map(lambda x: x.split("/")[0],topo_data[1].strip().split(";"))
-        topo_data[2] = topo_data[2].strip().split(";")
-        if topo_data[3]:
-             topo_data[3] = topo_lambda(topo_data[3].strip())
+    # topology_calculated = 0
+    # topo_data = [[],[],[],"",""]
+    # topo_file = glob.glob(base_url + "/topology.txt")
+    # if topo_file:
+    #     topology_calculated = 1
+    #     topo_file = topo_file[0]
+    #     with open(topo_file) as topo_in:
+    #         topo_data = topo_in.readlines()
+    #     topo_data[0] = map(topo_lambda,topo_data[0].strip().split(";"))
+    #     topo_data[1] = map(lambda x: x.split("/")[0],topo_data[1].strip().split(";"))
+    #     topo_data[2] = topo_data[2].strip().split(";")
+    #     if topo_data[3]:
+    #          topo_data[3] = topo_lambda(topo_data[3].strip())
     # print(topo_file)
     # end topology
 
@@ -212,6 +230,9 @@ def details(request, subfamily_id):
                                                           'family_msa_url': family_msa_url,
                                                           'subfamily_id': subfamily_id,
                                                           'cases_text': cases_text,
+                                                          'full_images': Full_alignment_images,
+                                                          'repeat_images': Repeat_alignment_images,
+                                                          'fa_len': fa_len,
                                                           'buttons_text': buttons_text})
 
 def get_browse(request):
@@ -227,7 +248,7 @@ def get_browse(request):
     headers = ["Subfamily", "Family", "Superfamily", "Fold-type","Topology", "Pfam"]
 
 
-    sub_families= [f.split("/")[-1] for f in glob.glob(settings.DATA_DIR + "/CPA/*") if os.path.isdir(f)]
+    sub_families= [f.split("/")[-1] for f in glob.glob(settings.DATA_DIR + "/CPA/*") if os.path.isdir(f) and "images" not in f]
     family_stats = []
 
 
